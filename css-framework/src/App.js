6126eb0c395e357ced7613c9420293d1a1b2c831
Frengky Sihombing <64943858+Franky777shg@users.noreply.github.com>
2022-09-28 11:57:56 +0700

    add new to do done

--- css-framework/src/App.js
+++ css-framework/src/App.js
@@ -3,20 +3,32 @@ import { Button, Heading, Center, Input, Flex, Stack } from "@chakra-ui/react";
 import ToDo from "./components/toDo";
 
 function App() {
-  const [lists, setLists] = useState(["Coding", "Eat", "Repent", "Sleep"]);
+  const [lists, setLists] = useState([]);
 
   const renderToDo = () => {
     return lists.map((item) => {
       return <ToDo data={item} />;
     });
   };
+
+  const onAdd = () => {
+    let newData = document.getElementById("newToDo").value;
+    // console.log(newData);
+    // let temp = [...lists];
+    // temp.push(newData);
+    // setLists(temp);
+    setLists([...lists, newData]);
+  };
+
   return (
     <Flex direction="column">
       <Heading textAlign="center" mb={5}>
         To Do List App
       </Heading>
-      <Input placeholder="Basic usage" />
-      <Button colorScheme="teal">Add</Button>
+      <Input placeholder="Add New To Do List" id="newToDo" />
+      <Button onClick={onAdd} colorScheme="teal">
+        Add
+      </Button>
       <Center>
         <Stack>{renderToDo()}</Stack>
       </Center>
